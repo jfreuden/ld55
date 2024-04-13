3e67067d0448874a9b8dfe0310d1eebfea730311
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2024-04-13 18:25:57 +0200

    Menu

---
+++ Ludum Dare 55/menu.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class menu : Control
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GetNode<Button>("%Play").GrabFocus();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+    public void PlayGame()
+    {
+        GetTree().ChangeSceneToFile("root.tscn");
+    }
+
+    public void Settings()
+    {
+        
+    }
+    
+    public void QuitGame()
+    {
+        GetTree().Quit();
+    }
+}
