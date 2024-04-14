807295598befcc6247c315a4c18a472ef1996992
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2024-04-15 01:53:24 +0200

    extracted player scene. Going to sleep

--- Ludum Dare 55/Player.cs
+++ Ludum Dare 55/Player.cs
@@ -1,13 +1,18 @@
 using Godot;
 using System;
 
-public partial class CharacterBody2D : Godot.CharacterBody2D
+public partial class Player : Godot.CharacterBody2D
 {
     [Export]
     public float Speed { get; private set; } = 50;
 
     [Export] public float Momentum { get; private set; } = 0.25f;
-    
+
+    [Signal]
+    public delegate void ItemAddedToHandEventHandler();
+
+    [Signal]
+    public delegate void ItemRemovedFromHandEventHandler(); 
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
